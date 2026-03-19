900b6c2c93c49f0a84281dd647289c12081cbf20
MAAUN/24/CBS/0010 <muhammadmahbubsani@gmail.com>
2026-03-18 20:21:33 -0400

    Create Assignment2model.py

---
+++ week4/Assignment2model.py
@@ -0,0 +1,32 @@
+from abc import ABC, abstractmethod
+
+# Abstract Base Class
+class StationAsset(ABC):
+    def __init__(self, name):
+        self.name = name
+
+    @abstractmethod
+    def calculate_revenue(self):
+        pass
+
+
+# Child Class 1
+class FuelDispenser(StationAsset):
+    def __init__(self, name, liters_sold, price_per_liter):
+        super().__init__(name)
+        self.liters_sold = liters_sold
+        self.price_per_liter = price_per_liter
+
+    def calculate_revenue(self):
+        return self.liters_sold * self.price_per_liter
+
+
+# Child Class 2
+class CarWash(StationAsset):
+    def __init__(self, name, cars_washed, price_per_car):
+        super().__init__(name)
+        self.cars_washed = cars_washed
+        self.price_per_car = price_per_car
+
+    def calculate_revenue(self):
+        return self.cars_washed * self.price_per_car
